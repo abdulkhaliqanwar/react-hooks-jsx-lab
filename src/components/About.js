567a08a56030418133baf8357872cdcba45248d1
abdulkhaliq anwar <abdulkhaliqanwar@student,moringaschool.com>
2025-01-15 00:50:22 +0300

    small change

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
   <div id="about">
     <h2>About Me</h2>
-    <p>Abdi is the best</p>
+    <p>Abdulkhaliq is the best</p>
     <img src = {image} alt="I made this"/>
 
   </div>
